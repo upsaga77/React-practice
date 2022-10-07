64d5412b59eabaafb8cdd187d5470fdc859de3cb
Suppawich Tawornpichayachai <upsaga77@hotmail.com>
2022-10-07 14:25:12 +0700

    Final implement

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./Create";
+import BlogDetails from "./ฺBlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content"></div>
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content"></div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/blogs/:id">
+            <BlogDetails />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,25 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
   //let name = "mario";
-  const [name, setName] = useState("mario");
-  const [blogs, setBlogs] = useState(null);
+  //const [name, setName] = useState("mario");
 
   /* const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   }; */
 
-  useEffect(() => {
-    fetch("http://localhost:3333/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-      });
-  }, []);
-
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:3333/blogs");
   return (
     <div className="home">
       <h2>Homepage</h2>
+      {error && <div> {error} </div>}
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
       {/* <button onClick={() => setName("mos")}>Change name</button>
       <p>{name}</p> */}
